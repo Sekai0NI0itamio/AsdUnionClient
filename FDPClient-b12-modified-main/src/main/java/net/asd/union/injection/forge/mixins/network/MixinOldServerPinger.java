25e918358c365d5e171fb32f1dc8d2b65757f215
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-04-11 11:46:51 +0800

    chore: sync vital files for remote build

--- FDPClient-b12-modified-main/src/main/java/net/asd/union/injection/forge/mixins/network/MixinOldServerPinger.java
+++ FDPClient-b12-modified-main/src/main/java/net/asd/union/injection/forge/mixins/network/MixinOldServerPinger.java
@@ -3,6 +3,8 @@ package net.asd.union.injection.forge.mixins.network;
 import io.netty.channel.Channel;
 import net.minecraft.client.network.OldServerPinger;
 import net.minecraft.network.NetworkManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,10 +17,42 @@ import java.util.List;
 
 @Mixin(OldServerPinger.class)
 public abstract class MixinOldServerPinger {
+    private static final Logger PING_LOG = LogManager.getLogger("ServerPingController");
 
     @Shadow
     @Final
     private List<NetworkManager> pingDestinations;
+
+    @Inject(method = "pingPendingNetworks", at = @At("HEAD"), cancellable = true)
+    private void fdp$pingPendingNetworksSafely(CallbackInfo ci) {
+        synchronized (this.pingDestinations) {
+            Iterator<NetworkManager> iterator = this.pingDestinations.iterator();
+
+            while (iterator.hasNext()) {
+                NetworkManager networkManager = iterator.next();
+
+                try {
+                    if (networkManager.isChannelOpen()) {
+                        networkManager.processReceivedPackets();
+                    } else {
+                        iterator.remove();
+                        networkManager.checkDisconnected();
+                    }
+                } catch (Throwable throwable) {
+                    iterator.remove();
+
+                    Channel channel = networkManager.channel();
+                    if (channel != null && channel.isOpen()) {
+                        channel.close();
+                    }
+
+                    PING_LOG.warn("Dropped a broken server ping request instead of crashing the multiplayer screen.", throwable);
+                }
+            }
+        }
+
+        ci.cancel();
+    }
 
     @Inject(method = "clearPendingNetworks", at = @At("HEAD"), cancellable = true)
     private void fdp$clearPendingNetworksAsync(CallbackInfo ci) {
